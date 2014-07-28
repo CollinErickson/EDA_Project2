586463a81d3e2b7a9df2ba09c0632a1b486e210b
CoolCoolCoolCool <collinberickson@gmail.com>
2014-07-27 19:11:53 -0500

    Added plot 6

--- plot5.R
+++ plot5.R
@@ -13,7 +13,7 @@ balt.years <- ddply(balt,'year',function(df){sum(df$Emissions)})
 
 png(filename='plot5.png')
 par(mfrow=c(1,1))
-plot(balt.years,pch=20,col='red',cex=2,main='Emissions from coal combustion-related sources 1999-2008',
+plot(balt.years,pch=20,col='red',cex=2,main='Emissions from motor vehicle sources in Baltimore 1999-2008',
      xlab='Year',ylab='Total emissions (tons)')
 abline(lm(balt.years$V1~balt.years$year))
 dev.off()
--- plot6.R
+++ plot6.R
@@ -2,12 +2,20 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 require(plyr)
-res <- ddply(NEI,'year',function(df){sum(df$Emissions)})
+
+balt <- NEI[NEI$type=='ON-ROAD' & NEI$fips=='24510',]
+balt.years <- ddply(balt,'year',function(df){sum(df$Emissions)})
+balt.p <- balt.years
+balt.p$Location <- rep('Baltimore',4)
+LA <- NEI[NEI$type=='ON-ROAD' & NEI$fips=='06037',]
+LA.years <- ddply(LA,'year',function(df){sum(df$Emissions)})
+LA.p <- LA.years
+LA.p$Location <- rep('LA County',4)
+
+toplot <- rbind(balt.p,LA.p)
 
 png(filename='plot6.png')
-#par(mfrow=c(1,1))
-#plot(res,pch=20,col='red',cex=2,main='Total emissions from PM2.5 from 1999 to 2008',
-#     xlab='Year',ylab='Total emissions (tons)')
-plot(rnorm(10))
-#abline(lm(res$V1~res$year))
+ggplot(toplot,aes(year,V1,color=Location))+geom_point(size=8)+geom_smooth(method=lm,se=T,lwd=2)+labs(title='Emissions from motor vehicle sources for\nLA County and Baltimore 1999-2008')+xlab('Year')+ylab('Total emissions (tons)')
 dev.off()
+
+
